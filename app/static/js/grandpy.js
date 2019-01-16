f4dcbc690fddcab83d8b159ef841fd565f145213
TimG <timoguic@gmail.com>
2019-01-16 15:29:31 +0100

    JS improvements

--- app/static/js/grandpy.js
+++ app/static/js/grandpy.js
@@ -3,27 +3,47 @@ let submitBtn = document.getElementById('form_submit_btn')
 
 gpForm.addEventListener('submit', (ev) => {
     let queryTxt = document.getElementById('form_input_txt').value
-    console.log(queryTxt)
     fetch("/api?query=" + queryTxt)
-      .then((resp) => resp.json())
-      .then((data) => {
-          let respElm = document.getElementById('bot_response')
+        .then((resp) => resp.json())
+        .then((data) => {
+            const respElm = document.getElementById('bot_response')
 
-          let qNode = document.createElement('span')
-          qNode.classList.add('question')
-          let rNode1 = document.createElement('span')
-          let rNode2 = document.createElement('span')
-          rNode1.classList.add('answer')
-          rNode2.classList.add('answer')
-          
-          qNode.innerText = queryTxt
-          rNode1.innerText = data['wiki_extract']
-          rNode2.innerText = data['coords'] + ' ' + data['wiki_url']
-          
-          respElm.insertBefore(qNode, respElm.firstChild)
-          respElm.insertBefore(rNode2, respElm.firstChild)
-          respElm.insertBefore(rNode1, respElm.firstChild)
-      })
+            // create question span
+            const qNode = document.createElement('span')
+            qNode.classList.add('question')
+            qNode.innerText = queryTxt
+
+            // create response span
+            let rNode = document.createElement('span')
+            rNode.classList.add('answer')
+            
+            // create map div
+            let mapNode = document.createElement('div')
+            // use timestamp for unique ID
+            const mapid = Date.now().toString()
+            mapNode.id = mapid
+            // fix height and width
+            mapNode.classList.add('mapbox')
+            
+            // add map node to response node
+            rNode.append(mapNode)
+            // add response text to response node
+            introText = data['wiki_extract'] != null ? data['wiki_extract'] : "Je n'ai rien trouvé à ce sujet :sad:"
+            
+            rNode.append(document.createElement('span').innerText = introText)
+
+            // insert question
+            respElm.insertBefore(qNode, respElm.firstChild)
+            // insert answer
+            respElm.insertBefore(rNode, respElm.firstChild)
+            
+            // manipulate map
+            const mymap = L.map(mapid).setView([data['coords'][0], data['coords'][1]], 13)
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(mymap);
+
+        })
     ev.preventDefault()
     return false
 })
