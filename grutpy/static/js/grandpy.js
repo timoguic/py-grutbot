5e7d478115a11218172c3628c8a8f721d4f41c15
TimG <timoguic@gmail.com>
2019-07-09 15:14:56 +0200

    js cloneNode FTW

--- grutpy/static/js/grandpy.js
+++ grutpy/static/js/grandpy.js
@@ -1,86 +1,6 @@
 let gpForm = document.getElementById('grandpy_form')
 let submitBtn = document.getElementById('form_submit_btn')
 
-function createRow(...extraClasses) {
-    const elm = document.createElement('div')
-    elm.classList.add('row')
-    extraClasses.forEach(cls => elm.classList.add(cls))
-    return elm
-}
-
-function createCol(...extraClasses) {
-    const elm = document.createElement('div')
-    elm.classList.add('col')
-    extraClasses.forEach(cls => elm.classList.add(cls))
-    return elm
-}
-
-function createWeatherNode(weatherData) {
-    // create image and text nodes
-    const weatherText = document.createElement('h6')
-    weatherText.classList.add('weather-description')
-    // weatherText.append(document.createTextNode('Météo: '))
-    
-    const weatherImg = document.createElement('img')
-    weatherImg.src = weatherData['icon']
-    weatherImg.classList.add('weather-icon')
-
-    weatherText.append(weatherImg)
-    weatherText.append(document.createTextNode(weatherData['temp'] + '°C, ' + weatherData['description']))
-    
-    return weatherText
-}
-
-function createQuestionNode(txtVal) {
-    // create question node
-    const qNodeRow = createRow()
-    const qNode = createCol('s9', 'm6', 'question')
-    qNode.innerHTML = '<p>' + txtVal + '</p>'
-    qNodeRow.append(qNode)
-
-    return qNodeRow
-}
-
-function createWikiDiv(wiki) {
-    introText = typeof wiki['extract'] !== 'undefined' ? wiki['extract'] : "<p>Désolé, je n'ai rien trouvé! :sad:</p>"
-    wikiDiv = document.createElement('div')
-    wikiDiv.innerHTML = introText
-    
-    return wikiDiv
-}
-
-function createWebcamDiv(webcam) {
-    const webcamDiv = document.createElement('div')
-    webcamDiv.classList.add('webcam')
-    
-    const webcamH6 = document.createElement('h6')
-    webcamH6.textContent = 'Webcam'
-    webcamDiv.append(webcamH6)
-    
-    const webcamImg = document.createElement('img')
-    webcamImg.src = webcam['img']
-    webcamDiv.append(webcamImg)
-    
-    const webcamP = document.createElement('p')
-    webcamP.innerText = webcam['location']
-    webcamP.classList.add('webcam-location')
-    webcamDiv.append(webcamP)
-
-    return webcamDiv
-}
-
-function createMapNode(mapId) {
-    const mapNodeWrapper = createCol('s12', 'm4')
-    
-    // create map div
-    const mapNode = document.createElement('div')
-    mapNode.classList.add('mapbox')
-    mapNode.id = mapId
-
-    mapNodeWrapper.append(mapNode)
-    return mapNodeWrapper
-}
-
 function updateMap(coords, mapId) {
     let lat = coords[0]
     let lon = coords[1]
@@ -97,51 +17,42 @@ function submitData(txtValue) {
         .then((data) => {
             console.log(data)
             const respElm = document.getElementById('bot_response')
-            
-            // insert question
-            questionNode = createQuestionNode(txtValue)
-            respElm.insertBefore(questionNode, respElm.firstChild)
+            const templateElms = document.querySelectorAll('#chat_template > .row')
+            let answerNode = templateElms[0].cloneNode(true)
+            let questionNode = templateElms[1].cloneNode(true)
+            console.log(templateElms)
+            questionNode.querySelector('p').innerText = txtValue
 
-            // create response row
-            const rNodeRow = createRow()
-    
-            const rNode = createCol('s11', 'offset-s1', 'm10', 'offset-m2', 'answer')
-            rNode.style.padding = "1em"
-            rNodeRow.append(rNode)
-    
-            let answerNodeWrapper = createRow()
-            rNode.append(answerNodeWrapper)
-    
-            // insert answer
-            respElm.insertBefore(rNodeRow, respElm.firstChild)
-            
+            respElm.insertBefore(questionNode, respElm.firstChild)
+            respElm.insertBefore(answerNode, respElm.firstChild)
+
             if (data['parsed'] != false) {
     
                 if (data['coords'] != false) {                    
                     // use timestamp for unique ID
                     const mapid = Date.now().toString()
-                    // create wrapper
-                    mapNodeWrapper = createMapNode(mapid)
-                    weatherDiv = createWeatherNode(data['weather'])
-                    mapNodeWrapper.append(weatherDiv)
-                    
-                    answerNodeWrapper.append(mapNodeWrapper)
+
+                    mapbox = answerNode.querySelector('.mapbox')
+                    mapbox.id = mapid
                     updateMap(data['coords'], mapid)
 
-                }
-    
-                answerText = createCol('s12', 'm8')
+                    answerNode.querySelector('.wiki').innerHTML = data['wiki']['extract']
 
-                answerText.append(createWikiDiv(data['wiki']))  
-                answerText.append(createWebcamDiv(data['webcam']))
-                
-                answerNodeWrapper.append(answerText)
-                
+                    answerNode.querySelector('.weather-icon').src = data['weather']['icon']
+                    answerNode.querySelector('h6').append(
+                        document.createTextNode(
+                            data['weather']['temp'] + '°C, ' + data['weather']['description']
+                        )
+                    )
+
+                    answerNode.querySelector('.webcam-location').innerText = data['webcam']['location']
+                    answerNode.querySelector('.webcam-img').src = data['webcam']['img']
+
+                }
             } else {
-                rNode.innerHTML = "<p>Désolé, je n'ai pas compris la question. C'en était une?</p>" + 
-                "<pre><small>" + data['message'] + "</pre>"
+                answerNode.innerHTML = "<p>Désolé, je n'ai pas compris la question. C'en était une?</p>" + 
+                "<pre><small>" + data['parsed'] + "</pre>"
             }
-            
         })
 }
 
